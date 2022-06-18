9cc12604d365f08c0d9a3576967fec92b075154d
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-06-18 21:58:30 +0300

    Removed_Persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,16 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from './contacts/contacts-reducer';
 
-const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-    logger,
-];
-
-// Saving items to Local Storage
-const itemsPersistConfig = {
-    key: 'items',
-    storage,
-    blacklist: ['filter'],
-}
+// const middleware = [...getDefaultMiddleware(), logger]
 
 // Configuring store
 const store = configureStore({
     reducer: {
-        contacts: persistReducer(itemsPersistConfig, contactsReducer),
+        contacts: contactsReducer,
     },
-    middleware,
+    // middleware,
     // devTools: process.env.NODE_ENV ==='development',
 })
 
-const persistor = persistStore(store);
-
-export default {store, persistor};
+export default store;
