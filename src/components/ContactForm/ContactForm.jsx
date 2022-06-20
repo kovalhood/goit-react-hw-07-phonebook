36ed34235e54abfd97c08c194dd62817150e0b8e
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-06-20 16:49:52 +0300

    Added_React_Phone_Field_Functionality

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,26 +12,44 @@ const ContactForm = () => {
   const { data: contacts } = useGetContactsQuery();
   const [addContact] = useAddContactMutation();
     
-  const handleChange = event => {
-    const { name, value } = event.currentTarget;
+  // const handleChange = event => {
+  //   console.log(event.currentTarget);
+  //   const { name, value } = event.currentTarget;
     
-    switch (name) {
-      case 'name':
-        setForm(prevForm => ({ ...prevForm, [name]: value }));
-        break;
+  //   switch (name) {
+  //     case 'name':
+  //       setForm(prevForm => ({ ...prevForm, [name]: value }));
+  //       break;
       
-      case 'phone': {
-        setForm(prevForm => ({ ...prevForm, [name]: value }));
-        break;
-      }
+  //     case 'phone': {
+  //       setForm(prevForm => ({ ...prevForm, [name]: value }));
+  //       break;
+  //     }
       
-      default:
-        return;
-    }
-  };
+  //     default:
+  //       return;
+  //   }
+  // };
+
+  const handleNameChange = event => {
+    const { name, value } = event.currentTarget;
+    setForm(prevForm => ({ ...prevForm, [name]: value }));
+  }
+  const handlePhoneChange = phoneValue => {
+    // console.log(phoneValue);
+    setForm(prevForm => ({ ...prevForm, phone: phoneValue }));
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
+
+    if (form.name === '') {
+      return console.log('no name')
+    }
+
+    if (form.phone === '') {
+      return console.log('no phone')
+    }
 
     if (contacts.find(contact => contact.name.toLowerCase() === form.name.toLowerCase())) {
       resetForm();
@@ -48,11 +66,11 @@ const ContactForm = () => {
 
   return <form onSubmit={handleSubmit} className={s.form}>
     <Label labelTitle={'Name'}>
-      <InputName name={form.name} onNameChange={handleChange}/>
+      <InputName name={form.name} onNameChange={handleNameChange}/>
     </Label>
     
     <Label labelTitle={'Number'}>
-      <InputNumber phone={form.phone} onNumberChange={handleChange} />
+      <InputNumber phone={form.phone} onNumberChange={handlePhoneChange} />
     </Label>
     
     <Button type={'submit'} title={"Add contact"} />
--- src/components/ContactForm/InputName/InputName.jsx
+++ src/components/ContactForm/InputName/InputName.jsx
@@ -6,12 +6,12 @@ const InputName = ({ name, onNameChange}) => {
       type="text"
       name="name"
       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      required
       placeholder='Enter name'
       value={name}
       onChange={onNameChange}
       className={s.input}
+      // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      // required
       // onKeyPress={(event) => {
       //   if (!/^[A-Za-z]+$/.test(event.key)) {
       //     event.preventDefault();
